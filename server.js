3ae488060b999be1e7c9158f0e8b7462ab7056fe
Ramzi <ramziinfo2001@gmail.com>
2022-06-11 16:44:01 +0100

    scheduled commit

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("connected to db successfully");
 });
+//app.use(express.static(path.join(__dirname,"../../client/build"))) ;
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.send("home routeee");
@@ -74,7 +76,7 @@ app.delete("/api/posts/:id", async (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "../my-app/public/images")
+        cb(null, "./public")
     },
     filename: (req, file, cb) => {
         //console.log(file);
@@ -87,8 +89,6 @@ const upload = multer({ storage: storage });
 //add product
 app.post("/addProducts", upload.single("product_image"), async (req, res) => {
     try {
-        /* 
-                console.log(req.body); */
         const product = new Product({
             name: req.body.name,
             image: req.file.originalname,
@@ -118,7 +118,7 @@ app.delete("/api/products/:id", async (req, res) => {
         if (deletedTask == null)
             res.status(404).json({ message: `No product with id ${req.params.id}` });
         else {
-            fs.unlink(`../my-app/public/images/${deletedProdFind.image}`, (err) => {
+            fs.unlink(`./public/${deletedProdFind.image}`, (err) => {
                 if (err)
                     throw err;
             });
@@ -151,24 +151,6 @@ app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/contactUs", contactRouter);
 app.use("/api/getOrders", getOrdersRouter);
-
-
-/*  app.get("/api/products/:id", (req, res) => {
-
-    let typedProduct = data.products.find((prod) => {
-        return prod.id === req.params.id;
-    })
-    if (typedProduct)
-        res.status(200).send(typedProduct);
-    else {
-        res.status(404).send({ message: "product not found" });
-    }
-});  */
-
-/*  app.get("/api/products", (req, res) => {
-    let { products } = data;
-    res.send(products);
-});  */
 
 app.use((err, req, res, next) => {          //middleware fct for handling errors
     res.status(500).send({ message: err.message });
